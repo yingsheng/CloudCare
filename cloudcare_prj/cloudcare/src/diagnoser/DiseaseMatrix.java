3d97de4f901477d49b0f03884659a16652c34109
Mengda Yang <mengda.yang@gmail.com>
2014-04-21 14:38:49 -0400

    add random data generator

--- cloudcare_prj/cloudcare/src/diagnoser/DiseaseMatrix.java
+++ cloudcare_prj/cloudcare/src/diagnoser/DiseaseMatrix.java
@@ -5,4 +5,28 @@ import java.util.*;
 public class DiseaseMatrix {
 	public HashMap<String, Integer[]> matrix;
 	public ArrayList<String> symptomIndex;
+
+	static public DiseaseMatrix genRandData(Integer row, Integer col) {
+		DiseaseMatrix answer = new DiseaseMatrix();
+		Random r = new Random(System.nanoTime());
+
+		for (int i = 0; i < col; ++i) {
+			String f = "f" + r.nextInt(col / 2 + 1);
+			String l = "$l" + r.nextInt(col / 2 + 1);
+			answer.symptomIndex.add(f + l);
+		}
+		
+		Integer pubIndex = r.nextInt(col);
+		
+		for(int i = 0; i < row; ++i){
+			String d = "d" + i;
+			Integer[] array = new Integer[col];
+			for(int j = 0; j < col; ++j){
+				array[j] = r.nextInt(2);
+			}
+			array[pubIndex] = 1;
+		}
+
+		return answer;
+	}
 }
