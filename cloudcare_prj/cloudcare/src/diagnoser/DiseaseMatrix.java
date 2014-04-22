436e44301370b0e8d58755e0ddfe5dadaf91df87
Mengda Yang <mengda.yang@gmail.com>
2014-04-22 09:08:19 -0400

    fixed duplicate symptom

--- cloudcare_prj/cloudcare/src/diagnoser/DiseaseMatrix.java
+++ cloudcare_prj/cloudcare/src/diagnoser/DiseaseMatrix.java
@@ -9,19 +9,25 @@ public class DiseaseMatrix {
 	static public DiseaseMatrix genRandData(Integer row, Integer col) {
 		DiseaseMatrix answer = new DiseaseMatrix();
 		Random r = new Random(System.nanoTime());
-
+		HashSet<String> symptomSet = new HashSet<String>(); 
+		String f = null;
+		String l = null;
 		for (int i = 0; i < col; ++i) {
-			String f = "f" + r.nextInt(col / 2 + 1);
-			String l = "$l" + r.nextInt(col / 2 + 1);
+			f = "f" + r.nextInt(col / 2 + 1);
+			l = "$l" + r.nextInt(col / 2 + 1);
+			while(symptomSet.contains(f + l)){
+				f = "f" + r.nextInt(col / 2 + 1);
+				l = "$l" + r.nextInt(col / 2 + 1);
+			}
 			answer.symptomIndex.add(f + l);
 		}
-		
+
 		Integer pubIndex = r.nextInt(col);
-		
-		for(int i = 0; i < row; ++i){
+
+		for (int i = 0; i < row; ++i) {
 			String d = "d" + i;
 			Integer[] array = new Integer[col];
-			for(int j = 0; j < col; ++j){
+			for (int j = 0; j < col; ++j) {
 				array[j] = r.nextInt(2);
 			}
 			array[pubIndex] = 1;
